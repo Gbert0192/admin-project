95e6ae4506073bf62d2e1c03ac9b860c160044a9
FelixImmanuel231110056 <felix.immanuel@Students.mikroskil.ac.id>
2025-05-30 23:51:41 +0700

    testing unauthorized page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
-  return <></>;
+import React from "react";
+import UnauthorizedPageContent from "./unauthorized/page";
+
+export default function HomePage() {
+  return <UnauthorizedPageContent />;
 }
