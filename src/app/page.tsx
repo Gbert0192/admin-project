d3589fa8639287b74b9927ce3554ab6359a175d8
Gbert0192 <gtiofandy@gmail.com>
2025-05-31 22:02:06 +0700

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ import React from "react";
 
 export default async function HomePage() {
   const session = await auth();
+
   return (
     <>
-      <h1>{JSON.stringify(session, null, 2)}</h1>
+      <h1>Hi, {JSON.stringify(session?.user.name, null, 2)}</h1>
     </>
   );
 }
