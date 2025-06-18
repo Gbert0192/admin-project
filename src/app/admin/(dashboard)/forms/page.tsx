d654d47ced40cdea140092c5e31127dca4a1264a
gilbert <gilbert.t@adaremit.co.id>
2025-06-18 14:20:49 +0700

    hhehe

---
+++ src/app/admin/(dashboard)/forms/page.tsx
@@ -0,0 +1,10 @@
+// interface PermissionPageProps {
+//   searchParams: Promise<Record<string, string | undefined>>;
+// }
+
+// const PermissionsPage = async ({ searchParams }: PermissionPageProps) => {
+//   const params = await searchParams;
+//   return <></>;
+// };
+
+// export default PermissionsPage;
--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -11,10 +11,13 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import {
+  BadgePercent,
   ChevronRight,
+  FileText,
   LayoutDashboard,
   Lock,
   Shield,
+  SquareChevronLeft,
   Target,
   TrendingUp,
   UserLock,
@@ -25,6 +28,7 @@ import Link from "next/link";
 import React from "react";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import { SidebarLinkItem } from "./sidebar-link-item";
 
 interface Item {
   title: string;
@@ -82,51 +86,13 @@ const superAdmin: Item[] = [
   },
 ];
 
-function SidebarLinkItem({
-  item,
-  isActive,
-}: {
-  item: Item;
-  isActive: boolean;
-}) {
-  return (
-    <SidebarMenuItem>
-      <SidebarMenuButton asChild className="p-0 max-h-10">
-        <Link
-          href={`/admin${item.url}`}
-          className={clsx(
-            "relative group/item flex items-center gap-3 px-4 py-3 hover:bg-slate-200 dark:hover:bg-slate-800/50 border border-transparent h-full rounded-xl",
-            { "bg-slate-200 dark:bg-slate-800/50": isActive }
-          )}
-        >
-          <div className="flex-shrink-0 p-2 rounded-lg bg-gradient-to-br from-blue-500/10 to-indigo-500/10 group-hover/item:from-blue-500/20 group-hover/item:to-indigo-500/20">
-            <item.icon className="w-4 h-4 sm:w-5 sm:h-5 text-blue-600 dark:text-blue-400" />
-          </div>
-
-          <div className="flex-1 min-w-0 flex items-center justify-between">
-            <span
-              className={clsx(
-                "text-sm sm:text-base font-semibold text-slate-800 dark:text-slate-200 group-hover/item:text-blue-600 dark:group-hover/item:text-blue-400 transition-colors duration-300",
-                { "text-blue-600 dark:text-blue-400": isActive }
-              )}
-            >
-              {item.title}
-            </span>
-
-            <ChevronRight
-              className={clsx(
-                "ml-2 w-3 h-3 sm:w-4 sm:h-4 text-slate-400 opacity-0 group-hover/item:opacity-100 transition-opacity duration-200",
-                { "opacity-100": isActive }
-              )}
-            />
-          </div>
-
-          <div className="absolute inset-0 rounded-xl bg-gradient-to-r from-blue-500/5 to-indigo-500/5 opacity-0 group-hover/item:opacity-100 transition-opacity duration-300 -z-10"></div>
-        </Link>
-      </SidebarMenuButton>
-    </SidebarMenuItem>
-  );
-}
+const form: Item[] = [
+  {
+    title: "Form",
+    url: "/forms",
+    icon: FileText,
+  },
+];
 
 export function AppSidebar() {
   const session = useSession();
@@ -141,6 +107,10 @@ export function AppSidebar() {
   );
 
   const filteredSuperAdminMenu = superAdmin.filter((item) =>
+    allowedRoutes.includes(item.url)
+  );
+
+  const filteredFormMenu = form.filter((item) =>
     allowedRoutes.includes(item.url)
   );
 
@@ -169,7 +139,6 @@ export function AppSidebar() {
                   isActive={pathname === `/admin${item.url}`}
                 />
               ))}
-
               {filteredFeatureMenu.length > 0 && (
                 <h1 className="text-sm sm:text-lg font-semibold text-gray-500 mb-0 px-0 h-auto">
                   Feature
@@ -182,13 +151,24 @@ export function AppSidebar() {
                   isActive={pathname === `/admin${item.url}`}
                 />
               ))}
-
               {filteredSuperAdminMenu.length > 0 && (
                 <h1 className="text-sm sm:text-lg font-semibold text-gray-500 mb-0 px-0 h-auto">
                   Super Admin
                 </h1>
               )}
               {filteredSuperAdminMenu.map((item) => (
+                <SidebarLinkItem
+                  key={item.title}
+                  item={item}
+                  isActive={pathname === `/admin${item.url}`}
+                />
+              ))}{" "}
+              {form.length > 0 && (
+                <h1 className="text-sm sm:text-lg font-semibold text-gray-500 mb-0 px-0 h-auto">
+                  Form
+                </h1>
+              )}
+              {form.map((item) => (
                 <SidebarLinkItem
                   key={item.title}
                   item={item}
